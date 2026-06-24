678b69e8056588d7a0bd6ef00fe49c9d8fd7e16f
Caio Gustavo <caiogggustavo49@gmail.com>
2026-06-23 22:17:33 -0300

    case 3 concluído

--- ProjetoSemana1E2/src/entities/Pedido.java
+++ ProjetoSemana1E2/src/entities/Pedido.java
@@ -7,23 +7,28 @@ import java.util.Set;
 
 public class Pedido {
 
+    private Integer idPedido;
     private Cliente cliente;
     private List<ItemPedido> itemPedido = new ArrayList<>();
     private Set<Pagamento> pagamentos = new HashSet<>();
 
-    public Pedido(Cliente cliente, List<ItemPedido> itemPedido, Set<Pagamento> pagamentos) {
+    public Pedido(Integer idPedido,Cliente cliente, List<ItemPedido> itemPedido) {
+        this.idPedido = idPedido;
         this.cliente = cliente;
         this.itemPedido = itemPedido;
-        this.pagamentos = pagamentos;
     }
-
+public Integer getIdPedido() {
+        return idPedido;
+}
     public Cliente getCliente() {
         return cliente;
     }
     public List<ItemPedido> getItemPedido() {
         return itemPedido;
     }
-
+public void setIdPedido(Integer idPedido) {
+        this.idPedido = idPedido;
+}
     public Set<Pagamento> getPagamentos() {
         return pagamentos;
     }
